14ee4fb1e9d0b06f42c74d0a8e8213a1d84fc4fb
Adrians29 <adrian.egl@hotmail.com>
2025-12-06 06:55:46 -0500

    Add isDepartmentNameValid method to Department Class

--- src/main/java/org/adrianegl/Department.java
+++ src/main/java/org/adrianegl/Department.java
@@ -5,4 +5,22 @@ public class Department {
     private String departmentName;
 
     private static int nextId = 1;
+
+    private static boolean isDepartmentNameValid(String departmentName) {
+        if (departmentName == null || departmentName.isEmpty()) {
+            return false;
+        }
+
+        for (int i = 0; i < departmentName.length(); i++) {
+            char letter = departmentName.charAt(i);
+            if (!Character.isLetter(letter)) {
+                return false;
+            }
+            else if (!Character.isSpaceChar(letter)) {
+                return false;
+            }
+        }
+        return true;
+
+    }
 }
