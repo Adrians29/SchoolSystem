1382c73f9162fa7f8b66ac81e93a51b5f578c7a0
Adrians29 <adrian.egl@hotmail.com>
2025-12-06 10:05:14 -0500

    Add fields to Student Class

--- src/main/java/org/adrianegl/Student.java
+++ src/main/java/org/adrianegl/Student.java
@@ -1,4 +1,13 @@
 package org.adrianegl;
 
+import java.util.List;
+
 public class Student {
+    private String studentId;
+    private Gender gender;
+    private Address address;
+    private Department department;
+    private Lis<Course> registeredCourses;
+
+    private static int nextId;
 }
