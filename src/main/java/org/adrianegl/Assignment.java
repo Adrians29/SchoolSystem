b7c41b6beff17742c33219a2711f5c35099d7fb9
Adrians29 <adrian.egl@hotmail.com>
2025-12-06 10:27:46 -0500

    Add Assignment Class fields

--- src/main/java/org/adrianegl/Assignment.java
+++ src/main/java/org/adrianegl/Assignment.java
@@ -1,4 +1,10 @@
 package org.adrianegl;
 
 public class Assignment {
+    private String assignmentId;
+    private String assignmentName;
+    private double weight;
+    private  List<Integer> scores;
+
+    private static int nextId;
 }
