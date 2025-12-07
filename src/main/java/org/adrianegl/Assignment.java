1755ae2441db312c107082891e0917522642b777
Adrians29 <adrian.egl@hotmail.com>
2025-12-07 08:13:12 -0500

    Add first method calcAssignmentAvg to Assignment Class and fix typo in fields

--- src/main/java/org/adrianegl/Assignment.java
+++ src/main/java/org/adrianegl/Assignment.java
@@ -1,10 +1,25 @@
 package org.adrianegl;
 
+import java.util.List;
+
 public class Assignment {
     private String assignmentId;
     private String assignmentName;
     private double weight;
-    private  List<Integer> scores;
+    private List<Integer> scores;
 
-    private static int nextId;
+    private static int nextId = 1;
+
+    /**
+     * Calculates the avg score of the assignment
+     * @return
+     */
+    public double calcAssignmentAvg() {
+        double sum = 0;
+        for (double score : scores) {
+            sum += score;
+        }
+
+        return sum / scores.size();
+    }
 }
