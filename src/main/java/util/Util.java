e4e3040b50723f5be852089cb42824860ead48a9
Adrians29 <adrian.egl@hotmail.com>
2025-12-10 07:24:56 -0500

    Add toTitleCase method in Util Class

--- src/main/java/util/Util.java
+++ src/main/java/util/Util.java
@@ -1,4 +1,27 @@
 package util;
 
 public class Util {
+
+    /**
+     * Converts each word in a string to title case.
+     * @param str the string that want to convert into title case
+     * @return the string in title case
+     */
+    public static String toTitleCase(String str) {
+        if (str == null || str.isBlank()) {
+            return str;
+        }
+
+        String[] words = str.trim().toLowerCase().split("\\s+");
+        str = "";
+
+        for (String word : words) {
+            word = word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase() + " ";
+            str += word;
+        }
+        str = str.trim();
+
+        return str;
+
+    }
 }
