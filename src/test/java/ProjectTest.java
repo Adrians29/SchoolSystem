f93041f7beb11ec018e1dc3761cd1a17a429ae97
Adrians29 <adrian.egl@hotmail.com>
2025-12-13 21:01:58 -0500

    Add Address Class unit testing

--- src/test/java/ProjectTest.java
+++ src/test/java/ProjectTest.java
@@ -1,10 +1,28 @@
+import org.adrianegl.Address;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
  class ProjectTest {
-    //Address Test
+
+     //Address Test
 
     @Test
+    @DisplayName("isPostalCodeValid: A1B2C3 -> true")
+    void testValidPostalCode1() {
+        assertTrue(Address.isPostalCodeValid("A1B2C3"));
+    }
 
+     @Test
+     @DisplayName("isPostalCodeValid: 1A2B3F -> false")
+     void testInvalidPostalCode1() {
+         assertFalse(Address.isPostalCodeValid("1A2B3F"));
+     }
+
+     @Test
+     @DisplayName("isPostalCodeValid: a1b2e3 -> true")
+     void testValidPostalCode2() {
+         assertTrue(Address.isPostalCodeValid("a1b2e3"));
+     }
 }
