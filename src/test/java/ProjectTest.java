18cde2653989846b6f912c83f7ef6faf427cb311
Adrians29 <adrian.egl@hotmail.com>
2025-12-13 21:14:28 -0500

    Add Department Class unit testing

--- src/test/java/ProjectTest.java
+++ src/test/java/ProjectTest.java
@@ -1,4 +1,5 @@
 import org.adrianegl.Address;
+import org.adrianegl.Department;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -25,4 +26,35 @@ import static org.junit.jupiter.api.Assertions.*;
      void testValidPostalCode2() {
          assertTrue(Address.isPostalCodeValid("a1b2e3"));
      }
-}
+
+     //Department Test
+
+     @Test
+     @DisplayName("isDepartmentNameValid: Computer Science -> true")
+     void testValidDepartmentName1() {
+         assertTrue(Department.isDepartmentNameValid("Computer Science"));
+     }
+
+     @Test
+     @DisplayName("isDepartmentNameValid: Business -> true")
+     void testValidDepartmentName2() {
+         assertTrue(Department.isDepartmentNameValid("Business"));
+     }
+
+     @Test
+     @DisplayName("isDepartmentNameValid: 123Cooking -> false")
+     void testInvalidDepartmentName() {
+         assertFalse(Department.isDepartmentNameValid("123Cooking"));
+     }
+
+     @Test
+     @DisplayName("testDepartmentIdAutoIncrement(): D01 -> D02")
+     void testDepartmentIdAutoIncrement() {
+        Department department1 = new Department("Chemistry");
+        Department department2 = new Department("Discrete Math");
+        assertEquals("D01", department1.getDepartmentId());
+        assertEquals("D02", department2.getDepartmentId());
+    }
+
+
+ }
